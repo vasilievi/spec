ed7b5f463f31677819aef52ddcc9047f159748b6
vasilievi <vasilievi@outlook.com>
2025-10-27 09:02:50 +0300

    Deploy website - based on 9074c6b26e860eaa1ed33d3ff6286a5940335b53

---
+++ assets/js/0cc61256.f15a70b4.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkspec=globalThis.webpackChunkspec||[]).push([[1999],{5982:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/spec/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
